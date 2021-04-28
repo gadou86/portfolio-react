3fc6ccc6bdcb97c8dbeb3c49975e2e56fcf7f242
Gad Dayan <70294896+gadou86@users.noreply.github.com>
2021-04-28 00:21:58 -0400

    portfolio with react

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,27 @@
+import { Link, useLocation } from "react-router-dom";
+
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="title">
+        <h1>
+          <p>gad dayan</p>
+          <p>Frontend developer</p>
+        </h1>
+        <Link to="about">
+          <button>More Info</button>
+        </Link>
+      </div>
+      <div className="person">
+        <img
+          src={`${process.env.PUBLIC_URL}/gad.png`}
+          alt="person picture"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
